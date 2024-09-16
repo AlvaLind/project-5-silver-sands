bdbbcaa3572e791f641c565a5abaf7b7e38e7a1c
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-09-16 18:57:50 +0200

    Create wine category model in product app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Represents a category for wines in the inventory.
+    Each category has a unique name. 
+    Order alphabetically based on 'name'.
+    """
+    # Fields 
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True, null=True, max_length=600)
+
+    class Meta: 
+        ordering =["name"]
+    
+    def __str__(self):
+        return self.name
