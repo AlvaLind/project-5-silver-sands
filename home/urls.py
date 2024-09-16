fb5465eb4a776232122159ad82d513dd6b5bada1
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-09-16 21:27:53 +0200

    Create about us view and url paths

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('our_story/', views.about_us, name='about_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def home(request):
     """ A view to return the home page """
     
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """ A view to return the about us or our story page """
+    
+    return render(request, 'home/about_us.html')
