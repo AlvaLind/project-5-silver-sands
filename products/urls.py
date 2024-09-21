8833e79de85d5081450b61efcd40873c5084fed2
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-09-21 16:03:35 +0200

    Update review delete url path

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<int:wine_id>/', views.product_details, name='product_details'),
-    path('/<int:wine_id>/review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
+    path('<int:wine_id>/review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
 ]
