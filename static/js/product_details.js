d2b2f5ad1f3028dc6784c8dd978ee1da7b8b19e4
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-09-22 15:49:01 +0200

    Add edit review button and js to handle the form submission

--- static/js/product_details.js
+++ static/js/product_details.js
@@ -8,3 +8,16 @@ function scrollReviews(direction) {
         reviewGrid.scrollBy({ left: scrollAmount, behaviour: 'smooth' });
     }
 }
+
+// Handle the edit review form, fill fields and execute edit_review view function
+function fillReviewForm(rating, comment, reviewId, wineId) {
+    // Fill in the rating, comment, and IDs into the edit review form
+    document.getElementById('id_rating').value = rating;
+    document.getElementById('id_comment').value = comment;
+    document.getElementById('reviewId').value = reviewId;
+    document.getElementById('wineId').value = wineId;
+
+    // Dynamically set the action URL for the form to point to and run the edit_review view
+    let reviewForm = document.getElementById('reviewForm');
+    reviewForm.setAttribute("action", `/products/${wineId}/review/${reviewId}/edit/`);
+}
