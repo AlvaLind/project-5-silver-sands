13ea5209f2dd1cf03951211411b674e2b8152fae
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-09-27 17:20:25 +0200

    Add checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 from django.conf import settings
 
 from .forms import OrderForm
@@ -8,6 +9,23 @@ from products.models import Wine
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'We are sorry, but there seems to be an issue\
+            with processing your payment. Please try again after a few minutes.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -16,7 +34,6 @@ def checkout(request):
     
     # If user submits checkout form
     if request.method == 'POST':
-        print("POST checkout form activated")
         
         bag = request.session.get('bag', {})
         form_data = {
@@ -74,6 +91,7 @@ def checkout(request):
             currency = settings.STRIPE_CURRENCY,
         )
         client_secret = intent.client_secret
+        print(intent)
         
     order_form = OrderForm()
     
