e43d517a266c1bc6bd25c5316b740a4c7272925b
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-10-01 01:02:42 +0200

    Add styling to management dashboard, add and edit product templates

--- silver_sands/urls.py
+++ silver_sands/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -12,4 +14,4 @@ urlpatterns = [
     path('checkout/',include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('management_dashboard/', include('management_dashboard.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
