c291b395532bc45fc7ecf005ae9b28a71199f08f
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-09-30 17:11:44 +0200

    Create confirmation emails

--- silver_sands/settings.py
+++ silver_sands/settings.py
@@ -171,3 +171,4 @@ STRIPE_CURRENCY = 'aud'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'silversands@gmail.com'
