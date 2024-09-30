d747091d091b3b34a20780d0f63a9be727a4a6da
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-09-30 16:31:33 +0200

    Add default delivery information and order history to profile template

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Wine
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from bag.contexts import bag_contents
 
 import stripe
@@ -70,7 +72,7 @@ def checkout(request):
                 except Wine.DoesNotExist:
                     messages.error(request, (
                         "One of the wines in your bag wasn't found in our database. "
-                        "Please call us for assistance!")
+                        "Please contact us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
@@ -78,7 +80,7 @@ def checkout(request):
             request.session['save_info'] = 'save-info' in request.POST # Save the users profile info to the session 
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(request, 'There was an error with your form. Please double check your information.')
+            messages.error(request, 'There was an error with completing the order. Please double check your information.')
                         
     else:
         bag = request.session.get('bag', {})
@@ -97,7 +99,24 @@ def checkout(request):
         client_secret = intent.client_secret
         print(intent)
         
-    order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.default_full_name,
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
     
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -117,15 +136,40 @@ def checkout_success(request, order_number):
     """
     Handle a users successful checkout
     """
+
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        order.user_profile = profile
+        order.save()
+    
+        if save_info:
+            profile_data = {
+                'default_full_name': order.full_name,
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+    
     messages.success(request, f'Congratulations your order was successful! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
+    
     if 'bag' in request.session:
         del request.session['bag']
+        
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
     }
+    
     return render(request, template, context)
--- profiles/models.py
+++ profiles/models.py
@@ -15,7 +15,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
     # Fields
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_full_name = models.CharField(max_length=40, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=15, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
