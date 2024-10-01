7876b38229c4f9eb7179583b62f10ab93ec47c3a
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-10-01 16:32:17 +0200

    Update navbar formation and navbar toggle functionality

--- static/js/base.js
+++ static/js/base.js
@@ -14,4 +14,36 @@ document.addEventListener("DOMContentLoaded", function() {
         form.classList.toggle('d-none');      // Hide/show using d-none class
         form.classList.toggle('d-block');     // Show as block element
     });
+
+    // Handle navbar toggle events
+    var navbarToggle = document.querySelector('.navbar-toggler');
+    var navbarIconsLogo = document.querySelector('.navbar-icons-logo');
+    var navbarIconsBasket = document.querySelector('.navbar-icons-basket');
+
+
+    navbarToggle.addEventListener('click', function() {
+        // Check if the navbar is expanded
+        if (navbarIconsLogo.classList.contains('d-none') && navbarIconsBasket.classList.contains('d-none')) {
+            navbarIconsLogo.classList.remove('d-none'); // Show logo when opened
+            navbarIconsBasket.classList.remove('d-none'); // Show basket when opened
+        } else {
+            navbarIconsLogo.classList.add('d-none'); // Hide logo when closed
+            navbarIconsBasket.classList.add('d-none'); // Hide basket when closed
+        }
+    });
+
+    // Bootstrap events for handling collapse
+    var navbarCollapse = document.getElementById('navbarText');
+
+    // Show icons when navbar is fully closed
+    navbarCollapse.addEventListener('hidden.bs.collapse', function () {
+        navbarIconsLogo.classList.remove('d-none');
+        navbarIconsBasket.classList.remove('d-none');
+    });
+
+    // Hide icons when navbar is fully opened
+    navbarCollapse.addEventListener('shown.bs.collapse', function () {
+        navbarIconsLogo.classList.add('d-none');
+        navbarIconsBasket.classList.add('d-none');
+    });
 });
