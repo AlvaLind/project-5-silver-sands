732d0ba5f8bd6603d06b266afae7f6fc1be56372
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-10-01 23:42:46 +0200

    Remove secret key and set debug to false

--- silver_sands/settings.py
+++ silver_sands/settings.py
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$2wqh!hvergzjpf(@zv-9d3a25u9a2*f++0$c_e)h7b_e+bu(6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1', 'silver-sands-estate.herokuapp.com']
 
