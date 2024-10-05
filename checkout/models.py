f49cecbca77cc496682b010facedf114cd56b8b8
AlvaLind <146857078+AlvaLind@users.noreply.github.com>
2024-10-05 18:40:38 +0200

    Add and migrate status field to order model and update order number generator

--- checkout/models.py
+++ checkout/models.py
@@ -11,6 +11,14 @@ from profiles.models import UserProfile
 
 # Create your models here.
 class Order(models.Model):
+    
+    ORDER_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('processing', 'Paid & Processing'),
+        ('shipped', 'Shipped'),
+        ('fulfilled', 'Fulfilled'),
+        ('returned', 'Returned & Refunded'),
+    ]
     # Relationships
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                 null=True, blank=True, related_name='orders')
@@ -32,12 +40,13 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    status = models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default='pending')
 
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
         """
-        return uuid.uuid4().hex.upper()
+        return f"ORD-{uuid.uuid4().hex[:8].upper()}"
 
     def update_total(self):
         """
